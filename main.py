e90692f2e140ca58b5345e8a8ada00cd8813caa9
NuminousBlue <jdiehl2236@gmail.com>
2023-03-31 06:02:21 -0500

    input

--- main.py
+++ main.py
@@ -3,25 +3,37 @@ import time as t
 import webbrowser
 import sys
 
-# Rewrite to remind user of anything!
 
 YEAR = 2023
 MONTH = 3
-HOUR = 18
+HOUR = 0
 MINUTE = 29
 
 
-days = ['mar_13', 'mar_14', 'mar_16', 'mar_20', 'mar_21']
-day_vals = [13, 14, 16, 20, 21]
+days = ['mar_13', 'mar_14', 'mar_16', 'mar_20', 'mar_21', 'mar_31']
+day_vals = [13, 14, 16, 20, 21, 31]
 urls = ['https://zoom.us/j/91027252569', 'https://zoom.us/j/96947278951',
-        'https://zoom.us/j/96442631204', 'https://zoom.us/j/96442631204', 'https://zoom.us/j/96442631204']
+        'https://zoom.us/j/96442631204', 'https://zoom.us/j/96442631204', 'https://zoom.us/j/96442631204', 'https://www.google.com/']
 
 assert(len(days) == len(day_vals) == len(urls))
 
 
+def get_sleep_duration():
+    return int(input("How often do you wish to poll the time?\n\n(Sleep duration in seconds) ::: "))
+
+
+def validate_input_args():
+    args = sys.argv
+    if len(args) > 2:
+        print("USAGE: <executable>")
+        return
+    return args
+
+
 def main():
-    timer = int(input(
-        "How often do you wish to poll the time?\n\n(Sleep duration in seconds) ::: "))
+    cl_input = validate_input_args()
+
+    sleep_duration = get_sleep_duration()
 
     print("|script running|")
     running = True
@@ -31,16 +43,22 @@ def main():
             date = dt.now().date()
             time = dt.now().time()
 
-            for details in make_sessions().values():
-                url, thyme = details
-                url = details[url]
-                thyme = details[thyme]
+            print(f"Polled for events @ {time}")
 
-                if date == thyme.date() and time == thyme.time():
+            sessions = make_sessions()
+
+            for details in sessions.values():
+                url, thyme = details
+                url, thyme = details[url], details[thyme]
+
+                if date == thyme.date() and time >= thyme.time():
+                    print(details)
+                    # del sessions[details]
                     webbrowser.open(url)
 
-            t.sleep(timer)
-        except (EOFError, RuntimeError, SystemError, SystemExit, KeyboardInterrupt):
+            t.sleep(sleep_duration)
+
+        except (EOFError, RuntimeError, SystemError, SystemExit, KeyboardInterrupt, StopIteration):
             print("The program was forced to terminate.")
             running = False
             sys.exit()
